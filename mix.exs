37df06861a38d3ef5e242cd9aa9d1c48cd4afb46
Nigel Williams-Lucas <nwilliams-lucas@triarii.olmypus.lan>
2018-09-07 23:45:00 -0400

    Added some configuration files

--- mix.exs
+++ mix.exs
@@ -1,15 +1,21 @@
 defmodule Sdp.MixProject do
+  @moduledoc false
   use Mix.Project
 
   def project do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.19.1", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10.0"},
+      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false}
+    ]
   end
 end
